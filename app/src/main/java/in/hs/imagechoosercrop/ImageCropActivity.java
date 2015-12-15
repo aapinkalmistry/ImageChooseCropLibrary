8b1647faac4c1a66cca41e87aa8168af221cf580
pinkalmistryaa <pinkal.mistry.aa@gmail.com>
2015-12-15 16:12:28 +0530

    Firt comit

---
+++ app/src/main/java/in/hs/imagechoosercrop/ImageCropActivity.java
@@ -0,0 +1,13 @@
+package in.hs.imagechoosercrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageCropActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_chooer_crop);
+    }
+}
